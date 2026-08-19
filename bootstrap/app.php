50f5aeb0c0801f1de8ab4d611e75041c56e7fc15
Wong Huey Teng <yellowcanstop@gmail.com>
2026-08-19 23:21:49 +0800

    Register api and FireFakeWebhook command.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
@@ -17,6 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
             ['verify.webhook' => VerifyWebhookSignature::class]
         );
     })
+    ->withCommands([
+        __DIR__ . '/../app/Console/Commands',
+    ])
 
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
